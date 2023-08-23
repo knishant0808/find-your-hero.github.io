aa9ad3cc2b41536ebeb92629eb126e4e812dd5bf
Nishant Kumar <37207638+knishant0808@users.noreply.github.com>
2023-08-23 13:45:15 +0530

    Added the feature to mark favourites and made the code more modular

--- assets/js/hero_detail.js
+++ assets/js/hero_detail.js
@@ -1,49 +1,60 @@
-let urlParams = new URLSearchParams(window.location.search);
-let heroId = urlParams.get('id');
+const urlParams = new URLSearchParams(window.location.search);
+const heroId = urlParams.get('id');
 
+// Constants for the API keys and URL endpoints
 const PUBLIC_KEY = "a9b98783ba0ed2096792b8b16aaafbe3";
 const PRIVATE_KEY = "2b6433a8cfc22599629305be3f970e284e1794e5";
 const hash = "a3b7640c225b2ffbeb55b329e8509682";
 const timeStamp = "1692715909981";
 const BASE_URL = 'https://gateway.marvel.com:443/v1/public/characters/';
 
-function fetchHeroDetails() {
-    return fetch(`${BASE_URL}${heroId}?ts=${timeStamp}&apikey=${PUBLIC_KEY}&hash=${hash}`)
-        .then(response => response.json())
-        .then(data => data.data.results[0])
-        .catch(error => {
-            console.error("There was an error fetching the hero details", error);
-        });
-}
-
-function populateBasicDetails(hero) {
-    document.getElementById('heroImage').src = `${hero.thumbnail.path}.${hero.thumbnail.extension}`;
-    document.getElementById('heroName').innerText = hero.name;
-    document.getElementById('heroDescription').innerText = hero.description || "Description not available";
-}
-
-function displayList(elementId, items) {
-    const listElement = document.getElementById(elementId);
-    if (items && items.length > 0) {
-        items.forEach(item => {
-            const listItem = document.createElement('li');
-            listItem.innerText = item.name || "N/A"; // In case some names are null or not provided
-            listElement.appendChild(listItem);
-        });
-    } else {
-        const listItem = document.createElement('li');
-        listItem.innerText = "Not available";
-        listElement.appendChild(listItem);
+// Service class to manage API calls
+class HeroDetailsService {
+    static fetchHeroDetails() {
+        return fetch(`${BASE_URL}${heroId}?ts=${timeStamp}&apikey=${PUBLIC_KEY}&hash=${hash}`)
+            .then(response => response.json())
+            .then(data => data.data.results[0])
+            .catch(error => {
+                console.error("There was an error fetching the hero details", error);
+            });
     }
 }
 
+// Module to manage the basic details of a hero
+class HeroDetail {
+    static populateBasicDetails(hero) {
+        document.getElementById('heroImage').src = `${hero.thumbnail.path}.${hero.thumbnail.extension}`;
+        document.getElementById('heroName').innerText = hero.name;
+        document.getElementById('heroDescription').innerText = hero.description || "Description not available";
+    }
+}
+
+// Module to display different lists related to the hero
+class HeroList {
+    static displayList(elementId, items) {
+        const listElement = document.getElementById(elementId);
+        if (items && items.length > 0) {
+            items.forEach(item => {
+                const listItem = document.createElement('li');
+                listItem.innerText = item.name || "N/A"; // Handle cases where some names might be null or not provided
+                listElement.appendChild(listItem);
+            });
+        } else {
+            const listItem = document.createElement('li');
+            listItem.innerText = "Not available";
+            listElement.appendChild(listItem);
+        }
+    }
+}
+
+// Initialization function
 function init() {
-    fetchHeroDetails().then(hero => {
-        populateBasicDetails(hero);
-        displayList('heroComics', hero.comics.items);
-        displayList('heroStories', hero.stories.items);
-        displayList('heroEvents', hero.events.items);
-        displayList('heroSeries', hero.series.items);
+    HeroDetailsService.fetchHeroDetails().then(hero => {
+        HeroDetail.populateBasicDetails(hero);
+        HeroList.displayList('heroComics', hero.comics.items);
+        HeroList.displayList('heroStories', hero.stories.items);
+        HeroList.displayList('heroEvents', hero.events.items);
+        HeroList.displayList('heroSeries', hero.series.items);
     });
 }
 
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,10 @@
+// Constants related to API keys and hashing
 const PUBLIC_KEY = "a9b98783ba0ed2096792b8b16aaafbe3";
 const PRIVATE_KEY = "2b6433a8cfc22599629305be3f970e284e1794e5";
 const hash = "a3b7640c225b2ffbeb55b329e8509682";
 const timeStamp = "1692715909981";
 
+// This class provides services for fetching superhero data from MARVEL's API.
 class SuperheroService {
     constructor(publicKey, privateKey, hash, timeStamp) {
         this.baseURL = "https://gateway.marvel.com:443/v1/public/characters";
@@ -12,17 +14,22 @@ class SuperheroService {
         this.timeStamp = timeStamp;
     }
 
+    // Fetches superheroes that start with the provided query name
     fetchSuperheroesByName(query) {
+        console.log("fetchinfg query data ...");
         const url = `${this.baseURL}?ts=${this.timeStamp}&apikey=${this.publicKey}&hash=${this.hash}&nameStartsWith=${query}`;
         return fetch(url).then(response => response.json());
     }
 
+    // Fetches a subset of superheroes, useful for getting random superheroes
     fetchRandomSuperheroes(offset, limit) {
+        console.log("fetching random data ...");
         const url = `${this.baseURL}?ts=${this.timeStamp}&apikey=${this.publicKey}&hash=${this.hash}&offset=${offset}&limit=${limit}`;
         return fetch(url).then(response => response.json());
     }
 }
 
+// This class manages the suggestion dropdown that appears as user types in the search box.
 class SuggestionBox {
     constructor(searchInput, suggestionsBox, superheroService) {
         this.searchInput = searchInput;
@@ -32,9 +39,11 @@ class SuggestionBox {
         this.init();
     }
 
+    // Set up event listeners for this class
     init() {
         this.searchInput.addEventListener("input", this.handleInput.bind(this));
-
+        
+        // Hide the suggestion box when clicking outside of it
         document.addEventListener("click", (e) => {
             if (e.target !== this.searchInput && e.target !== this.suggestionsBox) {
                 this.suggestionsBox.innerHTML = "";
@@ -42,53 +51,93 @@ class SuggestionBox {
         });
     }
 
+    // Handle the input event, fetch matching superheroes and display them
     handleInput() {
+        clearTimeout(this.debounceTimer); // Clear any existing timer
+
         const query = this.searchInput.value;
         if (query.length < 2) {
             this.suggestionsBox.innerHTML = "";
             return;
         }
-
-        this.superheroService.fetchSuperheroesByName(query).then(data => {
-            this.displaySuggestions(data.data.results);
-        });
+        
+        // Set a delay of 300ms before triggering the API call
+        this.debounceTimer = setTimeout(() => {
+            this.superheroService.fetchSuperheroesByName(query).then(data => {
+                this.displaySuggestions(data.data.results);
+            });
+        }, 300);
     }
 
+    // Render the fetched superheroes as suggestions
     displaySuggestions(heroes) {
         this.suggestionsBox.innerHTML = "";
         heroes.slice(0, 5).forEach(hero => {
-            const suggestionItem = document.createElement("a");
-            suggestionItem.href = `hero-detail.html?id=${hero.id}`;
+            const suggestionItem = document.createElement("div");
             suggestionItem.className = "list-group-item list-group-item-action suggestion-item";
-
+    
+            // The hero link
+            const heroLink = document.createElement("a");
+            heroLink.href = `hero-detail.html?id=${hero.id}`;
+            heroLink.className = "hero-link"; // This will help in styling, if needed.
+    
             const heroImg = document.createElement("img");
             heroImg.className = "suggestion-img";
             heroImg.src = `${hero.thumbnail.path}.${hero.thumbnail.extension}`;
-            suggestionItem.appendChild(heroImg);
-
+            heroLink.appendChild(heroImg);
+    
             const heroName = document.createTextNode(hero.name);
-            suggestionItem.appendChild(heroName);
+            heroLink.appendChild(heroName);
+            
+            suggestionItem.appendChild(heroLink);
+    
+            // The favorite icon
+            const favIcon = document.createElement("i");
+            favIcon.className = "fas fa-heart favorite-icon";
+            if(isFavorite(hero.id)) {
+                favIcon.className = "fas fa-heart favorite-icon";
+            }else {
+                favIcon.className = "far fa-heart favorite-icon"
+                favIcon.dataset.heroId = hero.id;
+            }
+            suggestionItem.appendChild(favIcon);
+    
             this.suggestionsBox.appendChild(suggestionItem);
         });
+    
+        // Add event listener for favorite icon click within the suggestion box
+        this.suggestionsBox.addEventListener('click', function(e) {
+            if (e.target && e.target.matches('.favorite-icon')) {
+                e.stopPropagation();
+                const heroId = e.target.dataset.heroId;
+                toggleFavorite(heroId);
+                // Update the icon class based on the new favorite status
+                e.target.className = isFavorite(heroId) ? "fas fa-heart favorite-icon" : "far fa-heart favorite-icon";
+            }
+        });
     }
+        
 }
 
+// This class is responsible for fetching and displaying random superheroes on the homepage.
 class RandomSuperheroes {
     constructor(displayArea, superheroService) {
         this.displayArea = displayArea;
         this.superheroService = superheroService;
     }
 
+    // Fetch and render random superheroes
     display() {
         const totalCharacters = 1500;
         const charactersPerRequest = 100;
         const randomOffset = Math.floor(Math.random() * (totalCharacters - charactersPerRequest));
-
+        
         this.superheroService.fetchRandomSuperheroes(randomOffset, charactersPerRequest).then(data => {
             this.showRandomSuperheroes(data.data.results);
         });
     }
 
+    // Render fetched superheroes on the webpage
     showRandomSuperheroes(superheroes) {
         this.displayArea.innerHTML = "";
         const randomIndices = [];
@@ -99,22 +148,53 @@ class RandomSuperheroes {
 
         randomIndices.forEach(index => {
             const hero = superheroes[index];
-            const heroDiv = `
-                <div class="col-md-4 random-hero">
-                    <a href="hero-detail.html?id=${hero.id}">
-                        <div class="image-wrapper">
-                            <img src="${hero.thumbnail.path}.${hero.thumbnail.extension}" alt="${hero.name}" class="random-hero-image">
-                        </div>
-                        <div class="hero-name">${hero.name}</div>
-                    </a>
-                </div>
+            const heroDiv = document.createElement('div');
+            heroDiv.className = "col-md-4 random-hero";
+            heroDiv.innerHTML = `
+                <a href="hero-detail.html?id=${hero.id}">
+                    <div class="image-wrapper">
+                        <img src="${hero.thumbnail.path}.${hero.thumbnail.extension}" alt="${hero.name}" class="random-hero-image">
+                        <i class="${isFavorite(hero.id) ? 'fas' : 'far'} fa-heart favorite-icon" data-hero-id="${hero.id}"></i>
+                    </div>
+                    <div class="hero-name">${hero.name}</div>
+                </a>
             `;
-
-            this.displayArea.innerHTML += heroDiv;
+        
+            // Add event listener for favorite icon within the heroDiv
+            const favoriteIcon = heroDiv.querySelector('.favorite-icon');
+            favoriteIcon.addEventListener('click', (event) => {
+                event.preventDefault(); // Prevent the anchor tag from navigating
+                const heroId = event.target.dataset.heroId;
+                toggleFavorite(heroId);
+                // Update the icon class based on the new favorite status
+                event.target.className = isFavorite(heroId) ? "fas fa-heart favorite-icon" : "far fa-heart favorite-icon";
+            });
+        
+            this.displayArea.appendChild(heroDiv);
         });
     }
 }
 
+
+// Helper function to toggle the favorite status in local storage
+function toggleFavorite(heroId) {
+    let favorites = JSON.parse(localStorage.getItem('favorites') || '[]');
+    if (favorites.includes(heroId)) {
+        const index = favorites.indexOf(heroId);
+        favorites.splice(index, 1);
+    } else {
+        favorites.push(heroId);
+    }
+    localStorage.setItem('favorites', JSON.stringify(favorites));
+}
+
+// Helper function to check if a hero is marked as a favorite
+function isFavorite(heroId) {
+    let favorites = JSON.parse(localStorage.getItem('favorites') || '[]');
+    return favorites.includes(heroId);
+}
+
+// Initialise services and components on page load
 document.addEventListener("DOMContentLoaded", function() {
     const superheroService = new SuperheroService(PUBLIC_KEY, PRIVATE_KEY, hash, timeStamp);
     const suggestionsBox = new SuggestionBox(document.getElementById("searchInput"), document.getElementById("suggestions"), superheroService);
